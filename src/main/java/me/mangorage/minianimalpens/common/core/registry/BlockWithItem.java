3e07ba16086f1045483dedbc62e97e213607441e
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2023-07-01 22:24:31 -0700

    Changed BWI (BlockWithItem.class)

--- src/main/java/me/mangorage/minianimalpens/common/core/registry/BlockWithItem.java
+++ src/main/java/me/mangorage/minianimalpens/common/core/registry/BlockWithItem.java
@@ -2,17 +2,17 @@ package me.mangorage.minianimalpens.common.core.registry;
 
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
-
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class BlockWithItem<B extends Block, I extends BlockItem> {
+    public static <B extends Block, I extends BlockItem> BlockWithItem<B, I> create(RegistryObject<B> Block, Function<RegistryObject<B>, RegistryObject<I>> Item) {
+        return new BlockWithItem<>(Block, Item);
+    }
+
     public final RegistryObject<I> ITEM;
     public final RegistryObject<B> BLOCK;
-    public BlockWithItem(RegistryObject<B> Block, Function<RegistryObject<B>, RegistryObject<I>> Item) {
+    private BlockWithItem(RegistryObject<B> Block, Function<RegistryObject<B>, RegistryObject<I>> Item) {
         this.BLOCK = Block;
         this.ITEM = Item.apply(Block);
     }
