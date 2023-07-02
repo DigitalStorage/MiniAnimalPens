2008717c48f9e936a96791d231178b516d875762
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2023-07-01 22:45:43 -0700

    Added BlockPen

--- src/main/java/me/mangorage/minianimalpens/MiniAnimalPensCore.java
+++ src/main/java/me/mangorage/minianimalpens/MiniAnimalPensCore.java
@@ -5,6 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 import static me.mangorage.minianimalpens.common.core.Constants.MOD_ID;
 
 @Mod(MOD_ID)
+@Mod.EventBusSubscriber()
 public class MiniAnimalPensCore {
     public MiniAnimalPensCore() {
         Registry.register();
--- src/main/java/me/mangorage/minianimalpens/common/core/registry/MAPBlocks.java
+++ src/main/java/me/mangorage/minianimalpens/common/core/registry/MAPBlocks.java
@@ -1,12 +1,13 @@
 package me.mangorage.minianimalpens.common.core.registry;
 
-import me.mangorage.minianimalpens.common.items.CustomBlockItem;
+import me.mangorage.minianimalpens.common.blocks.PenBlock;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 import static me.mangorage.minianimalpens.common.core.Constants.MOD_ID;
 
@@ -14,23 +15,18 @@ public class MAPBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
     public static final DeferredRegister<Item> ITEMS = MAPItems.ITEMS;
 
-    public static final BlockWithItem<Block, CustomBlockItem> BWI_TEST = BlockWithItem.create(
-            BLOCKS.register(
-                    "test",
-                    () -> new Block(
-                            BlockBehaviour.Properties.of()
-                    )
-            ),
-            (B) -> {
-                return ITEMS.register(
-                        "test",
-                        () -> new CustomBlockItem(
-                                B.get(),
-                                new Item.Properties()
-                        )
-                );
-            }
+    public static final RegistryObject<PenBlock> PEN_BLOCK = BLOCKS.register(
+            "animal_pen",
+            () -> new PenBlock(
+                    BlockBehaviour.Properties.of()
+            )
     );
 
-
+    public static final RegistryObject<BlockItem> PEN_BLOCK_ITEM = ITEMS.register(
+            "animal_pen",
+            () -> new BlockItem(
+                    PEN_BLOCK.get(),
+                    new Item.Properties().stacksTo(65)
+            )
+    );
 }
